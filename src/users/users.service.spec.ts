4ec95634da15c12b5c0765da2fe62433589d21cd
alphazero-wd <alphazero25811@gmail.com>
2022-07-15 13:05:34 +0700

    unit test

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
 
 describe('UsersService', () => {
   let service: UsersService;
-
+  let findOne: jest.Mock;
   beforeEach(async () => {
+    findOne = jest.fn();
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: { findOne },
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
@@ -14,5 +23,28 @@ describe('UsersService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('when getting a user by email', () => {
+    describe('and the user is matched', () => {
+      let user: User;
+      beforeEach(() => {
+        user = new User();
+        findOne.mockResolvedValue(user);
+      });
+      it('should return the user', async () => {
+        const fetchedUser = await service.getByEmail('test@test.com');
+        expect(fetchedUser).toEqual(user);
+      });
+    });
+
+    describe('and the user is not matched', () => {
+      beforeEach(() => {
+        findOne.mockResolvedValue(undefined);
+      });
+      it('should throw an error', async () => {
+        await expect(service.getByEmail('test@test.com')).rejects.toThrow();
+      });
+    });
   });
 });
