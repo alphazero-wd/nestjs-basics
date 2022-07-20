885cbc1b5403fc327652fade9383d0bf5f9a914a
alphazero-wd <alphazero25811@gmail.com>
2022-07-20 08:50:05 +0700

    fix repository

--- nestjs-project/src/chat/chat.module.ts
+++ nestjs-project/src/chat/chat.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { AuthModule } from '../auth/auth.module';
 import { ChatGateway } from './chat.gateway';
 import { ChatService } from './chat.service';
 import { Message } from './entities/message.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Message]), AuthModule],
-  providers: [ChatGateway, ChatService],
+  imports: [AuthModule, TypeOrmModule.forFeature([Message])],
+  providers: [ChatGateway, ChatService, Repository],
 })
 export class ChatModule {}
